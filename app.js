73e798e94f65562f3fd1bef1066c15dd59136fc9
Cindi Harris <cmr0319@gmail.com>
2017-08-14 10:33:04 -0400

    added all associated files that have been created for project

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let express = require('express');
+let handlebars = require('express-handlebars');
+const db = require("./db")
+
+
+
+
+
+
+
+
+//boilerplate
+db.connect(url, (err, connection) => {
+  if(!err)
+    console.log('connected to Mongo');
+  app.listen(3000, ()=>
+console.log('up and running');)
+})
